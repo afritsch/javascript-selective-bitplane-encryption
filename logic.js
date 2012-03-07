a42239426d6b1d14970ac9f93b74787187334d4f
unknown <Fluffy@.(none)>
2012-03-07 10:07:39 +0100

    added canvas with lenna

--- logic.js
+++ logic.js
@@ -1,1 +1,12 @@
-a=1;
+$(document).ready(function(){
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	context.b
+	
+	img = new Image();
+	img.src = "lenna.png";
+	canvas.width = img.width;
+	canvas.height =img.height;
+	
+	context.drawImage(img,0,0);
+});
