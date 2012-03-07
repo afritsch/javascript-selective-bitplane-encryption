6bd0fde5a1a98b4e9f24e783fd77d7025296f9e3
Andreas Fritsch <fritschandreas@hotmail.com>
2012-03-07 11:31:00 +0100

    bitplane set 0

--- logic.js
+++ logic.js
@@ -12,21 +12,26 @@ $(document).ready(function() {
 	var imageData = context.getImageData(0, 0, img.width, img.height);
 	
 	for (i = 0; i < imageData.width*imageData.height*4; i += 4) {        
-        
-    var red = imageData.data[i];	  
-    var green = imageData.data[i+1];	  
-    var blue = imageData.data[i+2];	 
-    var alpha = imageData.data[i+3];
     
-    var average = (red+green+blue)/3;
+    var binaryR, binaryG, binaryB;
     
-    imageData.data[i] = average;	  
-    imageData.data[i+1] = average;	  
-    imageData.data[i+2] = average;	 
-    imageData.data[i+3] = alpha;
+    binaryR = imageData.data[i].toString(2).split('');	  
+    binaryG = imageData.data[i+1].toString(2).split('');	  
+    binaryB = imageData.data[i+2].toString(2).split('');	 
+    
+    binaryR[0] = binaryG[0] = binaryB[0] = '0';
+    binaryR[1] = binaryG[1] = binaryB[1] = '0';
+    
+    binaryR = binaryR.join('');
+    binaryG = binaryG.join('');
+    binaryB = binaryB.join('');
+    
+    imageData.data[i] = parseInt(binaryR, 2);
+    imageData.data[i+1] = parseInt(binaryG, 2);
+    imageData.data[i+2] = parseInt(binaryB, 2);
 	}
 	
-	context.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height); 
-	
-	//alert(alpha.toString(2));
+	context.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height);
+  
+  console.log('fertig');
 }); 
