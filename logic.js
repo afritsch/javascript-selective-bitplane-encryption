b40b3ce4e035cae6a914ac62eb6ef2e77569ce77
unknown <Fluffy@.(none)>
2012-03-07 10:20:23 +0100

    getting rgba channels

--- logic.js
+++ logic.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");	
-	context.b
 	
 	img = new Image();
 	img.src = "lenna.png";
 	canvas.width = img.width;
-	canvas.height =img.height;
-	
+	canvas.height = img.height;
+
 	context.drawImage(img,0,0);
-});
+	var imageData = context.getImageData(0,0, img.width, img.height);
+	
+	for (i=0; i<imageData.width*imageData.height*4; i+=4){        
+        var red = imageData.data[i];	  
+        var green = imageData.data[i+1];	  
+        var blue = imageData.data[i+2];	 
+        var alpha = imageData.data[i+3];   
+	}
+}); 
