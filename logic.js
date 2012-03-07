f7a2d05825dcda34e14c28c456e5a3cb8eeabdc9
Andreas Fritsch <fritschandreas@hotmail.com>
2012-03-07 10:59:17 +0100

    formatting

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");	
 	
@@ -7,24 +8,25 @@ $(document).ready(function(){
 	canvas.width = img.width;
 	canvas.height = img.height;
 
-	context.drawImage(img,0,0);
-	var imageData = context.getImageData(0,0, img.width, img.height);
+	context.drawImage(img, 0, 0);
+	var imageData = context.getImageData(0, 0, img.width, img.height);
 	
-	for (i=0; i<imageData.width*imageData.height*4; i+=4){        
-        var red = imageData.data[i];	  
-        var green = imageData.data[i+1];	  
-        var blue = imageData.data[i+2];	 
-        var alpha = imageData.data[i+3];
+	for (i = 0; i < imageData.width*imageData.height*4; i += 4) {        
         
-        var average = (red+green+blue)/3;
-        
-        imageData.data[i] = average;	  
-        imageData.data[i+1] = average;	  
-        imageData.data[i+2] = average;	 
-        imageData.data[i+3] = alpha;
+    var red = imageData.data[i];	  
+    var green = imageData.data[i+1];	  
+    var blue = imageData.data[i+2];	 
+    var alpha = imageData.data[i+3];
+    
+    var average = (red+green+blue)/3;
+    
+    imageData.data[i] = average;	  
+    imageData.data[i+1] = average;	  
+    imageData.data[i+2] = average;	 
+    imageData.data[i+3] = alpha;
 	}
 	
-	context.putImageData(imageData,0,0,0,0, imageData.width,   imageData.height); 
+	context.putImageData(imageData, 0, 0, 0, 0, imageData.width, imageData.height); 
 	
 	//alert(alpha.toString(2));
 }); 
