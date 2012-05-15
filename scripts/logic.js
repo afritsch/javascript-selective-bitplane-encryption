b372b65cd7414b48f5772d4711ba0527c68ed362
Andreas Fritsch <fritschandreas@hotmail.com>
2012-05-15 08:52:46 +0200

    change

--- scripts/logic.js
+++ scripts/logic.js
@@ -166,7 +166,7 @@ function replacementAttack(numberOfCanvas, selectedBits, replacementAttackMode) 
 		//adding the lumiance value of the hole picture to the result of the replacementattack
     if(replacementAttackMode == 0 || replacementAttackMode == 1){
       
-			if(!replacementAttackMode){
+			if(parseInt(replacementAttackMode)){
 				luminanceCorrection*=-1;
 			}
 
